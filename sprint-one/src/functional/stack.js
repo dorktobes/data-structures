f4b56ace790579c111122b87da74afea654753cf
dorktobes <t.hoshman@gmail.com>
2017-10-11 15:10:32 -0700

    implemented functional 'stack push'

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,24 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    if (Object.keys(storage).length === 0) {
+      storage[0] = value;
+    } else {
+      var max = 0;
+      for (var prop in storage) {
+        if (Number(prop) > max) {
+          max = Number(prop);
+        }
+      }
+      storage[max + 1] = value;
+    }
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
