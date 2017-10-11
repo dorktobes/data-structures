774662b21e32e7bfe3f19f156540baf4ae6db106
dorktobes <t.hoshman@gmail.com>
2017-10-11 15:25:09 -0700

    finished enqueue in queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (Object.keys(storage).length === 0) {
+      storage[0] = value;
+    } else {
+      var max = 0;
+      for (keys in storage) {
+        if (Number(keys) > max) {
+          max = Number(keys);
+        }
+      }
+      storage[max + 1] = value;
+    }
+    
   };
 
   someInstance.dequeue = function() {
+    
+    
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
