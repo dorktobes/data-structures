0dfdf4cc16311bfef88e689eab86f7b187bf4722
dorktobes <t.hoshman@gmail.com>
2017-10-12 11:32:11 -0700

    implemented tree.js and calculated complexity of methods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,39 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
+  
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var childInstance = Tree(value);
+  this.children.push(childInstance);
 };
 
 treeMethods.contains = function(target) {
+  var exists = false;
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        exists = true;
+      }
+    }
+  } 
+  
+  return exists;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild's complexity is O(1);
+ contain's complexity is O(n);
+ 
  */
