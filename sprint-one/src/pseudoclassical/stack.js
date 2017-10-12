cd083f4b757d52005f2a1ba380628cefe8c9b132
dorktobes <t.hoshman@gmail.com>
2017-10-11 19:51:33 -0700

    finished implementation of pseudoclassical instantiation of stack.js

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,37 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function (value) {
+  if (Object.keys(this.storage).length === 0) {
+    this.storage[0] = value;
+  } else {
+    var max = 0;
+    for (var key in this.storage) {
+      if (Number(key) > max) {
+        max = Number(key);
+      }
+    }
+    this.storage[max + 1] = value;
+  }
+};
+Stack.prototype.pop = function () {
+  if (Object.keys(this.storage).length === 0) {
+    console.log('err');
+  } else {
+    var max = 0;
+    for (var key in this.storage) {
+      if (Number(key) > max) {
+        max = Number(key);
+      }
+    }
+    var lastEl = this.storage[max];
+    delete this.storage[max];
+    return lastEl;
+  }
+};
+
+Stack.prototype.size = function () {
+  return Object.keys(this.storage).length;
+};
 
