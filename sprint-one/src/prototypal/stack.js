333a4fb521c76db93cc84092a9cbaa54ff38f2ef
dorktobes <t.hoshman@gmail.com>
2017-10-11 17:39:12 -0700

    finished prototypal instantiation of stack.js

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,12 +1,13 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  return Object.create(stackMethods);
+  var stackInstance = Object.create(stackMethods);
+  stackInstance.storage = {};
+  return stackInstance;
   
 };
 
 var stackMethods = {
-  storage: {},
   push: function(value) {
     if (Object.keys(this.storage).length === 0) {
       this.storage[0] = value;
@@ -22,7 +23,25 @@ var stackMethods = {
     
     
   },
-  pop: function() {},
+  pop: function() {
+    if (Object.keys(this.storage).length === 0) {
+      console.log('err');
+    } else {
+      var max = 0;
+      for (var key in this.storage) {
+        if (Number(key) > max) {
+          max = Number(key);
+        }
+      }
+      var lastEl = this.storage[max];
+      console.log(this.storage);
+      delete this.storage[max];
+      console.log(this.storage);
+      return lastEl;
+    }
+    
+  },
+  
   size: function() {
     return Object.keys(this.storage).length;
   }
