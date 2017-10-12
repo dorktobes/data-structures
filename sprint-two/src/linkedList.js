3621fcd726255b30d8260fa4aa670f4d4eaa1671
dorktobes <t.hoshman@gmail.com>
2017-10-12 11:05:06 -0700

    added implementation of linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,18 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.head === null) {
+      var firstHead = Node(value);
+      list.head = firstHead;
+      list.tail = firstHead;
+    } else {
+      var nodeInstance = Node(value);
+      list.tail.next = nodeInstance;
+      list.tail = nodeInstance;
+    }
   };
 
   list.removeHead = function() {
+    //input value : none;
+    //return value: the head of the linked list
+    var thisHeadVal = list.head.value;
+    list.head = list.head.next;
+    return thisHeadVal;
+    //side effects: linked list head is reassigned to the address at the head's next property.
   };
 
   list.contains = function(target) {
+    for (var node in list) {
+      if (list[node].value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
